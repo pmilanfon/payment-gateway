c7d5e66fc0ce062ca9faef8882aa08f94cdf4601
pmilan <pmilan.fon@gmail.com>
2024-04-23 11:48:12 +0200

    tmp

--- src/main/java/com/payce/paymentgateway/processor/rest/CashierController.java
+++ src/main/java/com/payce/paymentgateway/processor/rest/CashierController.java
@@ -1,17 +1,28 @@
 package com.payce.paymentgateway.processor.rest;
 
+import com.payce.paymentgateway.processor.service.DepositService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api/payments/form")
+@AllArgsConstructor
 public class CashierController {
+
+    private final DepositService depositService;
 
     @GetMapping("/{txRef}")
     public String getCashierForm(@PathVariable String txRef) {
         return "forward:/form/index.html";
+    }
+
+    @PostMapping
+    public ResponseEntity<String> submitCardDetails(@RequestBody CardDetailsSubmit cardDetailsSubmit) {
+        depositService.submitCardDetails(cardDetailsSubmit);
+        return new ResponseEntity<>("Deposit submitted successfully", HttpStatus.OK);
     }
 }
 
